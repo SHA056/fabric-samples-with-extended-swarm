da79f1df5ff70fe4cdcfc616561922da554b015a
Shailaja Mahara <shailaja.mahara@gmail.com>
2019-09-20 17:14:16 +0530

    (delete) removed old server

--- server/buildFabric.sh
+++
@@ -1,7 +0,0 @@
-printf "\nStart with \"node registerAdmin.js\", then \"node registerUser.js\", then \"npm start\"\n\n"
-
-node registerAdmin.js
-
-node registerUser.js
-
-npm start
--- server/models/RefundGenerator.js
+++
@@ -1,9 +0,0 @@
-const mongoose= require('mongoose');
-const refundGenerator = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    credit: String,
-    limit: String,
-    handlingCharge: String,
-    updatedbyCategory: String
-});
-module.exports= mongoose.model('RefundGenerator',refundGenerator);
